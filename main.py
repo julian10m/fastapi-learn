307c399b9157dc8a2c771290e1206b065d49f075
Julian M. Del Fiore <j.d12f90@gmail.com>
2026-03-04 12:10:36 +0100

    Query parameters and String Validations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator
 
 FAKE_ITEMS_DB = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
@@ -16,15 +17,68 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return { 'message': 'Hello World!' }
 
-@app.get('/items') # e.g. /items/?skip=1&limit=1
-async def read_items(skip: int = 0, limit: int = 10):
-    return FAKE_ITEMS_DB[skip : skip + limit]
+@app.get('/items') # e.g. /items/?skip=1&limit=1&item-query=Loveaaaaaaaa
+async def read_items(
+    skip: int = 0, 
+    limit: int = 10, 
+    q: Annotated[str | None, Query(
+        alias="item-query", 
+        min_length=10, 
+        max_length=50, 
+        pattern="^L",
+        deprecated=True,
+    )] = None, # optional
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+):
+    data = { 'items': FAKE_ITEMS_DB[skip : skip + limit] } 
+    
+    if q:
+        data.update({ 'q': q })
+
+    if id:
+        data.update({ 'id': id })
+    
+    return data 
+
+@app.get("/items2/")
+async def read_items2(
+    q: Annotated[
+        str | None, 
+        Query(
+            title="This was written by Julian",
+            # alias='whatever',
+            description="A nice description here",
+            min_length=3
+        )
+    ]
+): # required but can be None
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get('/items_list') # e.g. items_list?q=abc&q=def
+async def read_items_list(q: Annotated[list[str] | None, Query()] = None): # optional list
+    query_items = {"q": q}
+    return query_items
+
+@app.get('/items_list_defaults') # e.g. items_list?q=abc&q=def
+async def read_items_list_defaults(
+    q: Annotated[list[str] | None, Query()] = ['abc', 'def']
+): # optional list, with defaults
+    query_items = {"q": q}
+    return query_items
 
 @app.post('/items')
 async def create_item(item: Item):
